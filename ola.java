331941109deb19f17d920f4acc5bc1cfee1ba0e2
Rodrigo <rodrigo_cnw@hotmail.com>
2017-05-27 08:20:33 -0300

    consegui

---
+++ ola.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)ola.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/5/27
+ */
+
+public class ola {
+        
+    
+    public static void main(String[] args) {
+        System.out.println("consegui");
+    }
+}
